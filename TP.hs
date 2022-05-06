ba7abbbab116a188a3efa269dc6375b82f1a6640
camiromero99 <camilaromero.c99@gmail.com>
2022-05-06 13:30:01 -0300

    Ya estan las correcciones

--- TP.hs
+++ TP.hs
@@ -6,14 +6,11 @@ nombreDelProducto (nombre, _) = nombre
 precioDelProducto :: Producto -> Float
 precioDelProducto (_,precio) = precio
 
--- En la funcion precioTotal me da error, yo supongo que es por los tipos! pero no logro ver el error 
-
---precioTotal :: Producto -> Float -> Float -> Float -> Float
---precioTotal producto cantidad descuento envio = aplicarCostoDeEnvio ((*cantidad). aplicarDescuento producto $ descuento)envio 
---precioTotal producto cantidad descuento envio = (+) (aplicarDescuento producto cantidad descuento) . (aplicarCostoEnvio envio) 
+precioTotal :: Producto -> Float -> Float -> Float -> Float
+precioTotal producto cantidad descuento envio = aplicarCostoDeEnvio ((*cantidad). aplicarDescuento producto $ descuento)envio 
 
 productoDeElite :: Producto -> Bool
-productoDeElite unProducto = productoDeLujo unProducto && productoCodiciado unProducto
+productoDeElite unProducto = productoDeLujo unProducto && productoCodiciado unProducto && not (productoCorriente unProducto)
 
 aplicarDescuento :: Producto -> Float -> Float
 aplicarDescuento unProducto descuento = precioDelProducto unProducto - descuento
@@ -22,19 +19,17 @@ entregaSencilla :: String -> Bool
 entregaSencilla unDia = even . length $ unDia
 
 descodiciarProducto :: Producto -> String
-descodiciarProducto unProducto = take 10 (nombreDelProducto  unProducto)
+descodiciarProducto unProducto = ((take 10 ).nombreDelProducto) unProducto
 
 productoDeLujo :: Producto -> Bool
 productoDeLujo unProducto = 'x' `elem` nombreDelProducto unProducto || 'z' `elem` nombreDelProducto unProducto
 
-aplicarCostoDeEnvio :: Producto -> Float -> Float
+aplicarCostoDeEnvio :: Float -> Float -> Float
 aplicarCostoDeEnvio unProducto precioDeEnvio = precioDelProducto unProducto + precioDeEnvio
 
-productoCodiciado' :: Producto -> Int
-productoCodiciado' unNombre = length . nombreDelProducto $  unNombre
-
 productoCodiciado :: Producto -> Bool
-productoCodiciado  unNombre = productoCodiciado' unNombre > 10
+productoCodiciado  unNombre = length.nombreDelProducto $ unNombre )>10
+    
 
 productoCorriente :: Producto -> Bool
 productoCorriente unProducto =  (primeraLetraVocal . nombreDelProducto) $ unProducto
@@ -43,7 +38,8 @@ primeraLetraVocal :: String -> Bool
 primeraLetraVocal nombre = (elem . head $ nombre) "aeiouAEIOU"
 
 productoXL :: Producto -> String
-productoXL unProducto = nombreDelProducto unProducto ++ "XL"
+productoXL unProducto = ((++) . nombreDelProducto $ unProducto ) "XL"
 
 versionBarata :: Producto -> String
 versionBarata unProducto = reverse. descodiciarProducto $ unProducto 
+
